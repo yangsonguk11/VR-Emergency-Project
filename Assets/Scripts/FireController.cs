19d2640130cb6b9c42077ffba737ad86a72d8b3d
yangsonguk11 <yangsonguk11@gmail.com>
2026-04-13 13:58:36 +0900

    Add FireTest
    Fire Controller는 inspector에서 추가한 ParticleSystem들의 Emission.RateOverTime을 수정하여 화력이 바뀌는 것을 표현함

---
+++ Assets/Scripts/FireController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireController : MonoBehaviour
+{
+    float fireIntensity;
+    public float FireIntensity { get { return fireIntensity; } set { fireIntensity = value; UpdateFire(); } }
+
+    [SerializeField] List<ParticleSystem> fireParticles;
+    List<int> fireParticlesorginalemission;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private void Start()
+    {
+        fireParticlesorginalemission = new List<int>();
+        foreach (ParticleSystem p in fireParticles)
+        {
+            fireParticlesorginalemission.Add(Mathf.RoundToInt(p.emission.rateOverTime.constant));
+        }
+        StartCoroutine(FireCor());
+    }
+    void UpdateFire()
+    {
+        for(int i = 0; i < 3; i++)
+        {
+            var emission = fireParticles[i].emission;
+            emission.rateOverTime = fireParticlesorginalemission[i] * FireIntensity;
+        }
+        foreach(ParticleSystem p in fireParticles)
+        {
+            Debug.Log(p.emission.rateOverTime);
+        }
+    }
+
+    IEnumerator FireCor()
+    {
+        float t = 0f;
+        
+        while(t < 8)
+        {
+            FireIntensity = 1f - t / 8f;
+            t += Time.fixedDeltaTime;
+            yield return new WaitForFixedUpdate();
+        }
+    }
+}
